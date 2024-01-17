ce1df35d006cb938f8f15d2281308c7aeab3f9d8
habib-SE <hab67603@gmail.com>
2024-01-17 21:26:38 +0500

    add data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
 import React from 'react';
-import PostList from './components/PostList';
+import DogGallery from './components/DogGallery';
 
 function App() {
   return (
     <div className="container mx-auto mt-8 p-4 bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">React App with Axios and Tailwind CSS Post Search By Id</h1>
-      <PostList />
+      <DogGallery />
     </div>
   );
 }
